ee2a2b18f7dbdb8d3b35d3985f1e98890d61020e
Igor Santos <532299+igorsantos07@users.noreply.github.com>
2023-08-23 22:05:21 -0300

    closes #11 Add Bootstrap + Clockify-related variables and layout :))

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,49 @@
+<style lang="scss">
+@import "./styles.scss";
+@import "../../node_modules/bootstrap-icons/font/bootstrap-icons";
+
+</style>
+
+<script>
+import { Collapse, Container, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Icon, Nav, Navbar, NavbarToggler, NavItem, NavLink } from 'sveltestrap'
+
+let isOpen = false
+const toggle = () => (isOpen = !isOpen)
+</script>
+
+<!-- color prop = bg; dark/light prop = text color...... lol -->
 <header>
-	<a href="/login">Login</a> <!-- FIXME add some sort of auth helper to identify if there's an API key available -->
-	<a href="/settings">Settings</a> <!-- TODO -->
-	<a href="/targets">Targets</a> <!-- TODO -->
-	<a href="/periods">Periods</a> <!-- TODO -->
+	<Navbar color="dark" dark expand="sm">
+		<!-- TODO move these to a sidebar menu, like the Clockify interface -->
+		<NavbarToggler class="border-0" on:click={toggle}/>
+		<Collapse {isOpen} navbar expand="sm">
+			<Nav navbar>
+				<NavItem>
+					<NavLink href="/login"><Icon name="box-arrow-in-left"/>Login</NavLink>
+				</NavItem> <!-- FIXME add some sort of auth helper to identify if there's an API key available -->
+				<NavItem>
+					<NavLink href="/settings"><Icon name="tools"/>Settings</NavLink> <!-- TODO -->
+				</NavItem>
+				<NavItem>
+					<NavLink href="/targets"><Icon name="bullseye"/>Targets</NavLink> <!-- TODO -->
+				</NavItem>
+				<NavItem>
+					<NavLink href="/periods"><Icon name="calendar"/>Periods</NavLink>  <!-- TODO -->
+				</NavItem>
+				<Dropdown nav inNavbar>
+					<DropdownToggle nav caret>About</DropdownToggle>
+					<DropdownMenu end>
+						<DropdownItem target="_blank" href="https://github.com/igorsantos07/clockify-monthly-tracker/issues"><Icon name="github"/>Source code</DropdownItem>
+						<DropdownItem target="_blank" href="https://github.com/igorsantos07/clockify-monthly-tracker/issues"><Icon name="bug"/>Issues</DropdownItem>
+						<DropdownItem divider/>
+						<DropdownItem target="_blank" href="http://www.igorsantos.com.br"><Icon name="eyeglasses"/>by @igorsantos07</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			</Nav>
+		</Collapse>
+	</Navbar>
 </header>
 
-<slot/>
-
-<footer>
-	by <a href="https://github.com/igorsantos07/" target="_blank">igorsantos07</a> |
-	<a href="https://github.com/igorsantos07/clockify-monthly-tracker" target="_blank">GitHub</a> |
-	<a href="https://github.com/igorsantos07/clockify-monthly-tracker/issues" target="_blank">Issues</a>
-</footer>
+<Container fluid class="py-3 px-0 px-sm-2">
+	<slot/>
+</Container>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 import API from '$lib/API.js'
+import { Button, Card, CardBody, CardFooter, CardHeader, CardTitle, FormGroup, FormText, Input } from 'sveltestrap'
 
 let key = window.localStorage.API_KEY || ''
+
 function doSave() {
 	window.localStorage.API_KEY = key
 
@@ -9,7 +11,19 @@ function doSave() {
 }
 </script>
 
-API Key:
-<input bind:value={key}/>
-<button on:click={doSave}>Save</button>
-<em>Grab this at the bottom of your <a href="https://app.clockify.me/user/settings" target="_blank">Profile Settings</a></em>
+<Card>
+	<CardHeader>
+		<CardTitle>Clockify API</CardTitle>
+	</CardHeader>
+	<CardBody>
+		<FormGroup floating label="API Key"><!-- TODO can't make this lg... -->
+			<Input bind:value={key} autofocus maxlength="48" minlength="48"/>
+		</FormGroup>
+		<FormText>
+			Grab this at the bottom of your <a href="https://app.clockify.me/user/settings" target="_blank">Profile Settings</a>
+		</FormText>
+	</CardBody>
+	<CardFooter class="text-end">
+		<Button color="primary" on:click={doSave}>Save</Button>
+	</CardFooter>
+</Card>
