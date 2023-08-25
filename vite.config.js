5193d890c98e53969ed498e2ced13ca035ce47f0
Igor Santos <532299+igorsantos07@users.noreply.github.com>
2023-08-25 18:46:33 -0300

    Add pipeline operator support + shut up annoying request for favicon.ico

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+import babel from 'vite-plugin-babel'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		babel({ babelConfig: {
+			babelrc   : false, //https://github.com/owlsdepartment/vite-plugin-babel#tips
+			configFile: false,
+			plugins   : [
+				['@babel/plugin-proposal-pipeline-operator', { proposal: 'hack', topicToken: '#' }],
+			]
+		}})
+	]
 });
