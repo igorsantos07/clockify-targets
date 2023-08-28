f920733ddb92c08e133ffe623c5cc92d377cbf2a
Igor Santos <532299+igorsantos07@users.noreply.github.com>
2023-08-28 20:33:51 -0300

    Care for unauthenticated accesses to protected pages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,6 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
-	}
+	},
+	reportUnusedDisableDirectives: true,
 };
--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,12 @@
+import { _store, get } from '$data/_store.js'
+import { redirect } from '@sveltejs/kit'
+import Auth from '$lib/Auth.js'
+
 export const ssr = false //TODO start using SSR, but first we need to wrap localStorage needs
+
+export function load({ route }) {
+	if ((!(new Auth().$isAuth) || !get(_store.user)) && route.id != '/api-key') {
+		throw redirect(302, '/api-key')
+	}
+	return {}
+}
