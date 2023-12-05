817b0331672e3d7019bf21c3394281ac3bbf7cd1
Igor Santos <532299+igorsantos07@users.noreply.github.com>
2023-12-05 01:12:59 -0300

    fix #24: enter triggers submit on login form

--- src/routes/api-key/+page.svelte
+++ src/routes/api-key/+page.svelte
@@ -22,20 +22,22 @@ function doClear() {
 </script>
 
 <Card>
-	<CardHeader>
-		<CardTitle>Clockify API</CardTitle>
-	</CardHeader>
-	<CardBody>
-		<p>First, you need to get your API key from Clockify, so we can talk to it and start doing the math for you <Icon name="emoji-smile"/></p>
+	<form on:submit|preventDefault={doSave}>
+		<CardHeader>
+			<CardTitle>Clockify API</CardTitle>
+		</CardHeader>
+		<CardBody>
+			<p>First, you need to get your API key from Clockify, so we can talk to it and start doing the math for you <Icon name="emoji-smile"/></p>
 
-		<!-- TODO can't make this lg... -->
-		<Linput label="API Key" bind:value={localKey} autofocus tabindex=1 required maxlength=48 minlength=48 />
-		<FormText>
-			Grab this at the bottom of your <a href="https://app.clockify.me/user/settings" target="_blank" tabindex=4>Profile Settings</a>.
-		</FormText>
-	</CardBody>
-	<CardFooter class="text-end">
-		<Button color="secondary" on:click={doClear} tabindex=3><Icon name="box-arrow-right"/> Clear that and log out</Button>
-		<Button color="primary" on:click={doSave} tabindex=2><Icon name="box-arrow-in-left"/> Let me in!</Button>
-	</CardFooter>
+			<!-- TODO can't make this lg... -->
+			<Linput label="API Key" bind:value={localKey} autofocus tabindex=1 required maxlength=48 minlength=48 />
+			<FormText>
+				Grab this at the bottom of your <a href="https://app.clockify.me/user/settings" target="_blank" tabindex=4>Profile Settings</a>.
+			</FormText>
+		</CardBody>
+		<CardFooter class="text-end">
+			<Button color="secondary" type="button" on:click={doClear} tabindex=3><Icon name="box-arrow-right"/> Clear that and log out</Button>
+			<Button color="primary" type="submit" on:click={doSave} tabindex=2><Icon name="box-arrow-in-left"/> Let me in!</Button>
+		</CardFooter>
+	</form>
 </Card>
