305accd0a6fb8bb15bad443f243a0a31ac03aff9
Igor Santos <532299+igorsantos07@users.noreply.github.com>
2023-12-06 03:44:13 -0300

    Upgrade Svelte's ESLint plugin and try to skip pesky tabindex / smart tab warnings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,41 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
+
+	extends: [
+		'eslint:recommended',
+		'plugin:svelte/recommended',
+	],
+
+	overrides: [{ files: ['*.svelte'], processor: 'svelte/svelte' }],
+
+	settings: {
+		svelte: {
+			kit: {
+				alias: {
+					$cmp : 'src/cmp',
+					$data: 'src/data',
+					'~'  : 'node_modules',
+				},
+			},
+		},
 	},
+
+	parserOptions: {
+		sourceType : 'module',
+		ecmaVersion: 2020,
+	},
+
 	env: {
 		browser: true,
-		es2017: true,
-		node: true
+		es2017 : true,
+		node   : true,
 	},
+
 	reportUnusedDisableDirectives: true,
-};
+
+	rules: {
+		// 'a11y-positive-tabindex': 'off',
+		'svelte/valid-compile'  : 'off', //seems to allow us to skip a11y-positive-tabindex properly?
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs']
+	},
+}
